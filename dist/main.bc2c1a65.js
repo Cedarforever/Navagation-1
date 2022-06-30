d7e45bc0f8ca25f2cabb1de8cceadf0b653b0d86
Cedar <cedarlzp@163.com>
2022-06-30 15:22:04 +0800

    add something

--- dist/main.bc2c1a65.js
+++ dist/main.bc2c1a65.js
@@ -109,7 +109,7 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 var xx = window.localStorage.getItem('x');
 var xxObject = JSON.parse(xx);
 
-var hashMap = [{ logo: 'A', url: 'https://www.acfun.cn' }, { logo: 'B', url: 'https://www.bilibili.com' }, { logo: 'D', url: 'https://developer.mozilla.org/zh-CN/' }, { logo: 'Z', url: 'https://www.zhihu.com' }, { logo: 'G', url: 'https://www.google.com' }, { logo: 'W', url: 'https://www.w3school.com.cn/' }, { logo: 'F', url: 'https://www.figma.com/' }, { logo: 'F', url: 'https://excalidraw.com/' }];
+var hashMap = xxObject || [{ logo: 'A', url: 'https://www.acfun.cn' }, { logo: 'B', url: 'https://www.bilibili.com' }, { logo: 'D', url: 'https://developer.mozilla.org/zh-CN/' }, { logo: 'Z', url: 'https://www.zhihu.com' }, { logo: 'G', url: 'https://www.google.com' }, { logo: 'W', url: 'https://www.w3school.com.cn/' }, { logo: 'F', url: 'https://www.figma.com/' }, { logo: 'F', url: 'https://excalidraw.com/' }];
 
 var simplifyUrl = function simplifyUrl(url) {
    return url.replace('https://', '').replace('http://', '').replace('www.', '').replace(/\/.*/, ''); //用了正则表达式  将/开头的所有内容  替换成空字符串''     
@@ -164,6 +164,5 @@ $(document).on('keypress', function (e) {
 $('.input').on('keypress', function (e) {
    e.stopPropagation();
 });
-localStorage.clear();
 },{}]},{},["epB2"], null)
-//# sourceMappingURL=main.1870c672.map
+//# sourceMappingURL=main.bc2c1a65.map
